405b8f2ef47eea4c79b05f3947d019724c06502c
Musakalamz <ogunsolumusa@gmail.com>
2025-11-04 11:07:00 +0100

    Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,29 +3,54 @@ import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
 import Services from "./Services";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   const navStyles = {
     display: "flex",
     gap: "20px",
     justifyContent: "center",
+    alignItens: "center",
+    backgroundColor: "#333",
+    padding: "15px",
+  };
+
+  const linkStyles = {
+    color: "white",
+    margin: "0 15px",
+    fontWeight: "bold",
+    textDecoration: "none",
   };
 
   return (
     <BrowserRouter>
-      <nav style={navStyles}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
+      <div>
+        <nav style={navStyles}>
+          <Link to="/" style={linkStyles}>
+            Home
+          </Link>
+          <Link to="/about" style={linkStyles}>
+            About
+          </Link>
+          <Link to="/services" style={linkStyles}>
+            Services
+          </Link>
+          <Link to="/contact" style={linkStyles}>
+            Contact
+          </Link>
+        </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <div style={{ marginTop: "60px" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+export default function Navbar() {
+  const navStyles = {
+    display: "flex",
+    gap: "20px",
+    justifyContent: "center",
+    aligbItens: "center",
+    backgroundColor: "#333",
+    padding: "15px",
+  };
+
+  const linkStyles = {
+    color: "white",
+    margin: "0 15px",
+    fontWeight: "bold",
+    textDecoration: "none",
+  };
+
+  return (
+    <nav style={navStyles}>
+      <Link to="/" style={linkStyles}>
+        Home
+      </Link>
+      <Link to="/about" style={linkStyles}>
+        About
+      </Link>
+      <Link to="/services" style={linkStyles}>
+        Services
+      </Link>
+      <Link to="/contact" style={linkStyles}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
