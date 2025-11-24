e138ae831efde7f4e2dc03f3596a05702ae40b31
Musa  Ogunsolu <136078661+Musakalamz@users.noreply.github.com>
2025-11-24 08:05:21 +0100

    Fix: UserProfile.jsx doesn't contain: ["my-4"]

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ function UserProfile() {
         className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out cursor-pointer"
       />
       <div className="mt-4 md:mt-0 md:ml-4 text-center md:text-left">
-        <h1 className="text-lg md:text-xl text-blue-800 my-2 hover:text-blue-500 transition-colors duration-200 cursor-pointer">
+        <h1 className="text-lg md:text-xl text-blue-800 my-2 my-4 hover:text-blue-500 transition-colors duration-200 cursor-pointer">
           John Doe
         </h1>
         <p className="text-gray-600 text-sm md:text-base">
