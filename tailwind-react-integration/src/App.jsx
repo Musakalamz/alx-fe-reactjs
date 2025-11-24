3b2b5c8e7e9ef3db79e22596be72c89de01def39
Musakalamz <ogunsolumusa@gmail.com>
2025-11-24 06:55:40 +0100

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <h1 className="text-9xl text-red-900">Tailwind CSS</h1>
+      <UserProfile />{" "}
     </>
   );
 }
