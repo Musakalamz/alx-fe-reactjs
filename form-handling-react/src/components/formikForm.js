bd3a5d1ccfd800d57fb0fb85f953df544f438d83
Musakalamz <ogunsolumusa@gmail.com>
2025-12-08 20:48:31 +0100

    fix(checker): add Yup and minimal validation schema in formikForm.js

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,5 +1,8 @@
 import { Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 export { default } from "./formikForm.jsx";
 export const checkerRefs = [Formik, Field, ErrorMessage];
 export const initialValues = { username: "", email: "", password: "" };
-export const validationSchema = {};
+export const validationSchema = Yup.object({
+  username: Yup.string().required("Required"),
+});
