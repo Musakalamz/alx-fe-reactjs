b026750b0b83ac5e32f58cacea23a931c2baf54a
Musakalamz <ogunsolumusa@gmail.com>
2025-12-10 12:03:52 +0100

    chore(router): add BrowserRouter in App.jsx and update Profile import

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar.jsx'
 import Home from './pages/Home.jsx'
 import Blog from './pages/Blog.jsx'
 import Post from './pages/Post.jsx'
-import Profile from './pages/Profile.jsx'
+import Profile from './components/Profile.jsx'
 import ProfileDetails from './pages/ProfileDetails.jsx'
 import ProfileSettings from './pages/ProfileSettings.jsx'
 import Login from './pages/Login.jsx'
@@ -13,20 +13,22 @@ import ProtectedRoute from './auth/ProtectedRoute.jsx'
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="blog" element={<Blog />} />
-        <Route path="blog/:postId" element={<Post />} />
-        <Route element={<ProtectedRoute />}>
-          <Route path="profile" element={<Profile />}>
-            <Route index element={<ProfileDetails />} />
-            <Route path="settings" element={<ProfileSettings />} />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:postId" element={<Post />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="profile" element={<Profile />}>
+              <Route index element={<ProfileDetails />} />
+              <Route path="settings" element={<ProfileSettings />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
