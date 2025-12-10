0dcf46db57f8f613a21fdd2e863ff86765de3c63
Musakalamz <ogunsolumusa@gmail.com>
2025-12-10 11:32:57 +0100

    feat(router): configure routes, nested, dynamic, and protected

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar.jsx'
+import Home from './pages/Home.jsx'
+import Blog from './pages/Blog.jsx'
+import Post from './pages/Post.jsx'
+import Profile from './pages/Profile.jsx'
+import ProfileDetails from './pages/ProfileDetails.jsx'
+import ProfileSettings from './pages/ProfileSettings.jsx'
+import Login from './pages/Login.jsx'
+import ProtectedRoute from './auth/ProtectedRoute.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="blog/:postId" element={<Post />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="profile" element={<Profile />}>
+            <Route index element={<ProfileDetails />} />
+            <Route path="settings" element={<ProfileSettings />} />
+          </Route>
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   )
 }
